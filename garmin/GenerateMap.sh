9d18c4fe729d678b98f50b656bc5cea964f489dc
Gustav Lindqvist <gustav@lindqvist.io>
2021-01-24 23:02:29 +0100

    More modifications to styling.

--- garmin/GenerateMap.sh
+++ garmin/GenerateMap.sh
@@ -7,8 +7,10 @@ STYLEFILE="C:\Users\Reedyn\Projects\GarminMaps\OpenTopoMapGarmin\garmin\style\op
 TYPFILETXT="C:\Users\Reedyn\Projects\GarminMaps\OpenTopoMapGarmin\garmin\style\typ\opentopomap.txt"
 TYPFILE="C:\Users\Reedyn\Projects\GarminMaps\OpenTopoMapGarmin\garmin\style\typ\opentopomap.typ"
 DATA="images/6324*.pbf"
-rm -r output
-java -jar $MKGMAPJAR -c $OPTIONS --style-file=$STYLEFILE --precomp-sea=$SEA --output-dir=output --bounds=$BOUNDS $DATA $TYPFILE
-mv "output/gmapsupp.img" "opentopomap_sweden_reedyn.img"
-rm -r output
-sleep 5
+
+java -cp $MKGMAPJAR uk.me.parabola.mkgmap.main.TypCompiler $TYPFILETXT $TYPFILE
+echo 'Compiled TYPFILE'
+rm -r "output/*"
+java -jar $MKGMAPJAR -c $OPTIONS --style-file=$STYLEFILE --family-name="Reedyn OSM Map ($(date +"%Y-%m-%d %H:%I"))" --precomp-sea=$SEA --output-dir=output --bounds=$BOUNDS $DATA $TYPFILE
+cp "output/gmapsupp.img" "OSM_Sweden_Reedyn.img"
+sleep 10
--- garmin/style/typ/GenerateTyp.sh
+++ garmin/style/typ/GenerateTyp.sh
@@ -3,4 +3,4 @@ MKGMAPJAR="C:\MapTools\mkgmap-r4594\mkgmap.jar"
 
 java -cp $MKGMAPJAR uk.me.parabola.mkgmap.main.TypCompiler opentopomap.txt opentopomap.typ
 echo 'Finished'
-sleep 10
+sleep 1
