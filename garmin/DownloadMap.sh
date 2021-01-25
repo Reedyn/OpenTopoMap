483b4834e0d489c697bac55de73e341ecd690c35
Gustav Lindqvist <gustav@lindqvist.io>
2021-01-25 19:11:01 +0100

    Removed names and points. Changed styling for highways.

---
+++ garmin/DownloadMap.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+rm -f sweden-latest.osm.pbf
+SWEDENPBFFILE="https://download.geofabrik.de/europe/sweden-latest.osm.pbf"
+wget $SWEDENPBFFILE
--- garmin/GenerateMap.sh
+++ garmin/GenerateMap.sh
@@ -12,5 +12,8 @@ java -cp $MKGMAPJAR uk.me.parabola.mkgmap.main.TypCompiler $TYPFILETXT $TYPFILE
 echo 'Compiled TYPFILE'
 rm -r "output/*"
 java -jar $MKGMAPJAR -c $OPTIONS --style-file=$STYLEFILE --family-name="Reedyn OSM Map ($(date +"%Y-%m-%d %H:%I"))" --precomp-sea=$SEA --output-dir=output --bounds=$BOUNDS $DATA $TYPFILE
+echo "Copying file to root"
+sleep 1
 cp "output/gmapsupp.img" "OSM_Sweden_Reedyn.img"
+echo "Done"
 sleep 10
